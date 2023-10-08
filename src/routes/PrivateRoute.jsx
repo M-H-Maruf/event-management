3d3d57d97a3969884776f720effa31fc55cbb777
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-10-09 00:51:21 +0600

    forgot password feature is commented out as per assignment instructions

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,27 +1,27 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { PropTypes } from 'prop-types';
+import { PropTypes } from "prop-types";
 import { AuthContext } from "../hooks/AuthProvider";
 
-
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext);
-    const location = useLocation();
-    
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
-    if (loading) {
-        return <span className="loading loading-infinity loading-lg"></span>
-    }
+  if (loading) {
+    return (
+      <span className="absolute top-[50vh] left-[50vw] loading loading-infinity loading-lg text-6xl text-accent -translate-x-1/2 -translate-y-1/2"></span>
+    );
+  }
 
-    if (user) {
-        return children;
-    }
+  if (user) {
+    return children;
+  }
 
-    return <Navigate state={location.pathname} to="/login"></Navigate>;
+  return <Navigate state={location.pathname} to="/login"></Navigate>;
 };
 
 PrivateRoute.propTypes = {
-    children: PropTypes.object
-}
+  children: PropTypes.object,
+};
 
 export default PrivateRoute;
